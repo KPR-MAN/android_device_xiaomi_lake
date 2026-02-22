bfbff2b2db47b2e6f27500a8113a5ec6acbe85a2
K-MAX <158112978+KPR-MAN@users.noreply.github.com>
2026-02-23 01:18:19 +0200

    Added Vendor boot recovery and set it to TRUE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,6 +98,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 TW_INCLUDE_FUSE_EXFAT := true
 TW_INCLUDE_FUSE_NTFS := true
+TW_VENDOR_BOOT_RECOVERY := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone28/temp"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
